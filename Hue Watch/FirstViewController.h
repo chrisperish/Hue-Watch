f6b56f527494b05ae8c9573e0f0c3e2ab544487d
Chris <Chris@Christophers-MacBook-Air.local>
2015-03-22 11:27:48 -0700

    Initial Commit

---
+++ Hue Watch/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Hue Watch
+//
+//  Created by Chris on 3/22/15.
+//  Copyright (c) 2015 Perish LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Hue Watch/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Hue Watch
+//
+//  Created by Chris on 3/22/15.
+//  Copyright (c) 2015 Perish LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
